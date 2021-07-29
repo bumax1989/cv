a88da3de3f51a13bab7c3b84f5a8ce24afb628da
Maksim Buchynski <61361054+bumax1989@users.noreply.github.com>
2021-07-29 23:44:09 +0300

    Add files via upload

--- docs/php/contact-form.php
+++ docs/php/contact-form.php
@@ -7,7 +7,7 @@ header('Content-type: application/json');
 require 'php-mailer/class.phpmailer.php';
 
 // Your email address
-$to = 'you@domain.com';
+$to = 'bumax1989@gmail.com';
 
 $subject = $_POST['subject'];
 
